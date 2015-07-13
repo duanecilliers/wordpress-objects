00afd9f6acede55911955fdefcc3402c2e01b465
Duane Cilliers <duanecilliers@gmail.com>
2015-07-13 18:39:37 +0200

    rename files to match class names and remove init file

--- src/wordpress-objects.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Duanec;
-
-include_once( 'wordpress-objects.post.php' );
-include_once( 'wordpress-objects.taxonomy.php' );
-include_once( 'wordpress-objects.term.php' );
-include_once( 'wordpress-objects.user.php' );
